263e68388d7450d26de343a18de4832ea6f7f3d1
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-05-28 15:35:45 -0300

    Commit (Funcao Logica) Cricao do formato inicial do APP.ts

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express"
+import { insereCliente } from "./controllers/clienteController"
+
+const app = express()
+const PORT = process.env.PORT ?? 3000
+app.use(express.json())
+
+function serverOn(){
+    console.log(`API está sendo executada no endereço: http:localhost:${PORT}`)
+}
