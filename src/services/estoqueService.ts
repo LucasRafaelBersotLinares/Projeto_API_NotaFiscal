e696fe208c7affe4cf99a901a21f365288b07cd0
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-05-29 05:22:22 -0300

    Commit (Funcao Logica) Criacao do formato inicial do estoque service.

---
+++ src/services/estoqueService.ts
@@ -0,0 +1,11 @@
+import { Estoque } from "../models/estoque"
+import { EstoqueRepository } from "../repositories/estoqueRepository"
+
+export class EstoqueService {
+    estoqueRepository: EstoqueRepository = EstoqueRepository.getInstance()
+
+    
+
+
+
+}
