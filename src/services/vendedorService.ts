6b9178550c2884e7c86c5c7dfec079200a8b19ac
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-05-29 00:28:34 -0300

    Commit (Reparo) Mudança na funcao de insereVendedor, colocando a regra de negocio de verificar comissao percentual.

--- src/services/vendedorService.ts
+++ src/services/vendedorService.ts
@@ -9,7 +9,10 @@ export class VendedorService {
             throw new Error("Dados obrigatórios faltantes!!! [Nome, Matricula, Comissao_percentual]")
         }
         if(this.vendedorRepository.matriculaRepetida(vendedorBody.matricula) === -1){
-            return this.vendedorRepository.insereVendedor(vendedorBody)
+            if(vendedorBody.comissao_percentual >= 0 && vendedorBody.comissao_percentual <= 30){
+                return this.vendedorRepository.insereVendedor(vendedorBody)
+            }
+            throw new Error("A comissão percentual tem que ser um valor entre 0 a 30")
         }
         throw new Error("Sistema ja possui um vendedor cadastrado nesta matricula")
     }
