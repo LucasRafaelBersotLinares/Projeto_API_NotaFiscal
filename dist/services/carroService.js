1c6e8ac200458c4ca9887d00921ee5161fb34242
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-05-29 02:37:29 -0300

    Commit (Reparo) Criada a regra de negocio de verificar o ano e preco. na posicao de verificar o ano, foi usada um metodo previamente visto na documentacao do typescript (getFullYear).

--- dist/services/carroService.js
+++ dist/services/carroService.js
@@ -5,11 +5,18 @@ const carroRepository_1 = require("../repositories/carroRepository");
 class CarroService {
     carroRepository = carroRepository_1.CarroRepository.getInstance();
     insereCarro(carroBody) {
+        const anoAtual = new Date();
         if (!carroBody.marca || !carroBody.modelo || !carroBody.ano || !carroBody.placa || !carroBody.preco || !carroBody.cor) {
             throw new Error("Dados obrigatórios faltantes!!!\n[Marca, Modelo, Ano, Placa, Preco, Cor]");
         }
         if (this.carroRepository.placaRepetida(carroBody.placa) === -1) {
-            return this.carroRepository.insereCarro(carroBody);
+            if (carroBody.ano >= 1950 && carroBody.ano <= (anoAtual.getFullYear() + 1)) {
+                if (carroBody.preco > 0) {
+                    return this.carroRepository.insereCarro(carroBody);
+                }
+                throw new Error("O preco do carro deve ser maior que zero");
+            }
+            throw new Error("O ano do carro deve ser mais que 1950 ou um ano valido antes do proximo ano atual (Ex: 2026 + 1 = 2027)");
         }
         throw new Error("Sistema ja possui um carro cadastrado nesta placa");
     }
--- src/services/carroService.ts
+++ src/services/carroService.ts
@@ -5,14 +5,20 @@ export class CarroService {
     carroRepository: CarroRepository = CarroRepository.getInstance()
 
     insereCarro(carroBody: any): Carro{
+        const anoAtual: Date = new Date()
+
         if(!carroBody.marca || !carroBody.modelo || !carroBody.ano || !carroBody.placa || !carroBody.preco || !carroBody.cor){
             throw new Error("Dados obrigatórios faltantes!!!\n[Marca, Modelo, Ano, Placa, Preco, Cor]")
         }
         if(this.carroRepository.placaRepetida(carroBody.placa) === -1){
-            return this.carroRepository.insereCarro(carroBody)
+            if(carroBody.ano >= 1950 && carroBody.ano <= (anoAtual.getFullYear()+1)){
+                if(carroBody.preco > 0){
+                    return this.carroRepository.insereCarro(carroBody)
+                }
+                throw new Error("O preco do carro deve ser maior que zero")
+            }
+            throw new Error("O ano do carro deve ser mais que 1950 ou um ano valido antes do proximo ano atual (Ex: 2026 + 1 = 2027)")
         }
         throw new Error("Sistema ja possui um carro cadastrado nesta placa")
     }
-
-
 }
