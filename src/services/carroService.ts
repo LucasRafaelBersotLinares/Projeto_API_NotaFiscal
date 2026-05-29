bd917af7907878c06940279ccca0becdbbfb7aa3
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-05-29 02:08:29 -0300

    Commit (Funcao Logica) Criacao do formato inicial do carro service.

---
+++ src/services/carroService.ts
@@ -0,0 +1,10 @@
+import { Carro } from "../models/carro"
+import { CarroRepository } from "../repositories/carroRepository"
+
+export class CarroService {
+    clienteRepository: CarroRepository = CarroRepository.getInstance()
+
+
+
+
+}
