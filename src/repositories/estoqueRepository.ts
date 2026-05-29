45b85c99fe6486106a0fe60f2b1227d9f59bb990
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-05-29 05:18:53 -0300

    Commit (Funcao Logica) Criacao do formato inicial do estoque repository.

---
+++ src/repositories/estoqueRepository.ts
@@ -0,0 +1,19 @@
+import { Estoque } from "../models/estoque"
+
+export class EstoqueRepository {
+    private static instance: EstoqueRepository
+    private estoqueList: Estoque[] = []
+    private constructor() {}
+
+    public static getInstance(): EstoqueRepository {
+        if(!this.instance){
+            this.instance = new EstoqueRepository()
+        }
+        return this.instance
+    }
+    
+
+
+
+
+}
