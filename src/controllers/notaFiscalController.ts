1b263e6c5f5c573d92dd34be1b0c2ee0533b2b29
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-05-29 19:02:55 -0300

    Commit (Funcao Logica) Criada a funcao de emiteNota no nota controller.

--- src/controllers/notaFiscalController.ts
+++ src/controllers/notaFiscalController.ts
@@ -2,3 +2,19 @@ import { Request, Response } from "express"
 import { NotaFiscalService } from "../services/notaFiscalService"
 
 const notaService = new NotaFiscalService()
+
+export function emiteNota(req: Request, res: Response){
+    try {
+        const newNota = notaService.emiteNota(req.body)
+        res.status(201).json(
+        {
+            message: "Nota emitida com sucesso!!!",
+            cliente: newNota
+        }
+        )
+    } catch( error: any){
+        res.status(400).json({message: error.message})
+    }
+
+
+}
